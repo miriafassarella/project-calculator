28e2984ff1220e5b61035e87335e6eb0c2e0b8a7
Miriã Fassarella <miriafassarella@gmail.com>
2019-06-27 10:46:47 -0300

    adicionando informaçoes no diaplay

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,6 +4,7 @@ class CalcController {
         
         //fazendo a amarração com o id do arquivo index.html(Modificando o html via javaScript).
         //manipulando o DOM
+        this._operation =[];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -27,13 +28,174 @@ class CalcController {
         
     }
 
-    addEventListenerAll(element, events, fn){ //criando meu método.
+    addEventListenerAll(element, events, fn){ //criando um método para tratar múltiplos eventos.
 
         events.split(' ').forEach(event =>{
 
             element.addEventListener(event, fn, false);
 //usamos o false para que o evento ocorra em apenas um elemento.
         });
+    }
+
+    clearAll(){
+
+        this._operation = [];
+
+    }
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    getLastOperation(){//extraindo a última posição do array.
+
+      return  this._operation[this._operation.length-1];
+
+    }
+
+    setLastOperation(value){
+
+        this._operation[this._operation.length -1] = value; //substitui o último item digitado.
+
+    }
+
+    isOperation(value){ //verificando se é um operador.
+
+      return ['+', '-', '*', '%', '/'].indexOf(value) > -1; //indexOf traz o index do elemento.
+       // se ele não encontrar retorna -1.
+    }
+    
+    pushOperation(value){
+
+        this._operation.push(value);
+
+        if(this._operation.length > 3){
+            
+            this.calc();
+
+        }
+
+    }
+
+    calc(){ //calculando a primeira operação digitada
+//utilizando os três primeiros elementos.
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
+
+    }
+
+    setLastNumberToDisplay(){
+
+        let lastNumber;
+
+        for(let i = this._operation.length-1; i >= 0; i--){
+
+            if(!this.isOperation(this._operation[i])){
+                
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+
+        this.displayCalc = lastNumber;
+    }
+
+    addOperation(value){
+
+       
+
+        if(isNaN(this.getLastOperation())){//se o útimo número digitado não é numérico
+
+            if(this.isOperation(value)){
+
+                this.setLastOperation(value); //substituindo o item.
+
+            }else if(isNaN(value)){
+
+                console.log(value);
+
+            }else{ 
+                this.pushOperation(value);//adicionando o elemento no array.
+
+                this.setLastNumberToDisplay();
+            }
+        }else{
+
+            if(this.isOperation(value)){
+
+                this.pushOperation(value);
+
+            }else{
+
+                let newValue = this.getLastOperation().toString() + value.toString(); //transformando o valor em uma string
+                this.setLastOperation(parseInt(newValue)); //retornando o valor para número.
+
+                this.setLastNumberToDisplay();
+            }
+
+           }
+}
+
+    setError(){
+
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+
+        switch (value) {//tratando as opções clicáveis
+
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                this.addOperation('+');
+                break;
+            case 'subtracao':
+                this.addOperation('-');
+                break;
+            case 'divisao':
+                this.addOperation('/');
+                break;
+            case 'multiplicacao':
+                this.addOperation('*');
+                break;
+            case 'porcento':
+                this.addOperation('%');
+                break;
+            case 'igual':
+
+                break;
+            case 'ponto':
+                this.addOperation('.');
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+
+        }
     }
 
     initButtonsEvents(){
@@ -46,8 +208,9 @@ class CalcController {
             this.addEventListenerAll(btn, "click drag", e => {// primeiro argumento refere-se ao evento e ...
                 //o segundo ao que deve ser feito.
 
-            console.log(btn.className.baseVal.replace("btn-", ""));
+            let textBtn = btn.className.baseVal.replace("btn-", "");
 
+            this.execBtn(textBtn);
                 });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
